380effca362bf944f101564f67ddb85b1e86b689
Wilmar28 <wrondonbarrero@gmail.com>
2026-03-17 19:38:31 -0500

    cambios pequeños

--- frontend/src/pages/CambiarContrasena.jsx
+++ frontend/src/pages/CambiarContrasena.jsx
@@ -32,14 +32,18 @@ function CambiarContrasena() {
 
     <div style={{
       maxWidth: "400px",
-      margin: "50px auto",
-      padding: "30px",
-      borderRadius: "10px",
+      margin: "125px auto",
+      padding: "35px",
+      borderRadius: "12px",
       background: "white",
       boxShadow: "0 5px 20px rgba(0,0,0,0.1)"
     }}>
 
-      <h2>Cambiar contraseña</h2>
+      <h2 style={{ 
+          marginBottom: "5px",
+          textAlign: "center",
+          color: "#2d3436"
+        }}>Cambiar contraseña</h2>
 
       <input
         type="password"
@@ -48,8 +52,12 @@ function CambiarContrasena() {
         onChange={(e) => setContrasena(e.target.value)}
         style={{
           width: "100%",
-          padding: "10px",
-          marginTop: "15px"
+          marginTop: "20px",
+          padding: "12px",
+          borderRadius: "8px",
+          border: "1px solid #dfe6e9",
+          marginBottom: "20px",
+          fontSize: "14px"
         }}
       />
 
@@ -57,12 +65,15 @@ function CambiarContrasena() {
         onClick={cambiar}
         style={{
           width: "100%",
-          marginTop: "20px",
-          padding: "10px",
-          background: "#2563eb",
-          color: "white",
-          border: "none",
-          borderRadius: "5px"
+            padding: "12px",
+            borderRadius: "8px",
+            border: "none",
+            background: "linear-gradient(to right, #0984e3, #74b9ff)",
+            color: "white",
+            fontWeight: "bold",
+            cursor: "pointer",
+            fontSize: "15px",
+            marginTop: "15px",
         }}
       >
         Actualizar contraseña
