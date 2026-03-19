6f022323c3560ac97aa8e4c62ae07e8c61a6e80c
Wilmar Rondon <wrondonbarrero@gmail.com>
2026-03-19 09:47:19 -0500

    imagen carga de dashboard

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'src', 'uploads')));
 // Test Route
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to DeviceGuard API' });
