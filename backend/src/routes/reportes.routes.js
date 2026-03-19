9b66bcc6aca0b46ad2c37e14ddc62fc62627487e
lemalu039-jpg <lema.lu039@gmail.com>
2026-03-19 07:38:57 -0500

    cambio de pdf a excel

--- backend/src/routes/reportes.routes.js
+++ backend/src/routes/reportes.routes.js
@@ -2,15 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  generarReporteUsuarios,
-  generarReporteDispositivos,
-  generarReporteBD
+  generarExcelUsuarios,
+  generarExcelDispositivos,
+  generarExcelBD
 } = require("../controllers/reportes.controller");
 
 
-router.get("/usuarios", generarReporteUsuarios);
-router.get("/dispositivos", generarReporteDispositivos);
-router.get("/bd", generarReporteBD);
+
+router.get("/usuarios-excel", generarExcelUsuarios);
+router.get("/dispositivos-excel", generarExcelDispositivos);
+router.get("/bd-excel", generarExcelBD);
 
 router.get("/", (req, res) => {
   res.send("REPORTES BASE FUNCIONA");
